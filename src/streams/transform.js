96e85db27e24762cd13308a03623bf0cc25395fc
elina <elina.nep@gmail.com>
2022-06-04 21:35:08 +0300

    feat: src/wt, src/cp

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -3,27 +3,25 @@ import path from 'path';
 import readline from 'readline';
 
 export const transform = async() => {
-  //   const outputFile = fs.createWriteStream(path.join('files', 'fileToWrite.txt'));
 
-  //   const rl = readline.createInterface({
-  //     input: process.stdin,
-  //     output: process.stdout,
-  //     prompt: 'Start your text here: \n'
-  //   });
-  //   rl.prompt();
-  //   rl.on('line', (line) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Start your text here: \n'
+  });
+  rl.prompt();
+  rl.on('line', (line) => {
 
 
-  //     outputFile.write(line + '\n');
-  //     console.log('All saved!');
-  //     outputFile.close();
-  //     process.exit(0);
+    console.log('All saved!');
 
-  //   }).on('close', () => {
-  //     outputFile.close();
-  //     process.exit(0);
+    process.exit(0);
 
-  //   });
+  }).on('close', () => {
+    outputFile.close();
+    process.exit(0);
+
+  });
 };
 
 transform()
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,36 @@
-export const performCalculations = async () => {
-    // Write your code here
+import path from 'path';
+import {
+  Worker,
+} from 'node:worker_threads';
+import os from 'os'
+
+export const performCalculations = async() => {
+  const cpuCount = os.cpus().length
+  const results = [];
+  for (let i = 0; i < cpuCount; i++) {
+    const worker = new Worker(path.resolve('worker.js'), { workerData: i + 10 })
+    const result = new Promise((resolve) => {
+      let data;
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          resolve({
+            status: 'error',
+            data: null
+          })
+        } else {
+          resolve({
+            status: 'resolved',
+            data
+          })
+        }
+      })
+      worker.on('message', (message) => {
+        data = message;
+      })
+    })
+    results.push(result);
+  }
+  return Promise.all(results)
 };
+
+performCalculations().then(results => console.log(results))
